3a9ec6b16ebda4e3f6c0e20908d0ccbb15b33b50
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-04-08 22:52:01 +0200

    Update content for script.js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,20 @@
 var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 
-add.addEventListener('click', function(e) {
    function checkOnclickEvent() {
     var element = document.createElement('li');
     var listLength = list.getElementsByTagName('li').length;
     for (i=0; i < listLength; i++) {
       element.innerHTML = "item " + listLength;
+
+      if (listLength == 10) {
+        alert("We've already got enough new items, it's time to stop!");
+        break;
+      }
     }
-    console.log(element.innerHTML);
+    //console.log(element.innerHTML);
     list.appendChild(element);
  }
-console.log(checkOnclickEvent());
-});
+
+checkOnclickEvent();
+add.addEventListener('click', checkOnclickEvent);
